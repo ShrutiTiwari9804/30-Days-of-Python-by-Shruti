e54bd7e31a418de7279eadda22377d79864dc625
Shruti Pankaj Tiwari <tiwarishruti105@gmail.com>
2026-07-06 09:56:29 +0530

    Password Strength Checker.

--- day 06/day06.py
+++ day 06/day06.py
@@ -1,33 +1,38 @@
-# ----------------------------------------------------------------------------
+# ----------------------------------------
 # Day 6 - Password Strength Analyzer
 # 30 Days of Python Challenge
-# -----------------------------------------------------------------------------
+# ----------------------------------------
 
 
 def check_length(password):
-    return len(password) >=8
+    return len(password) >= 8
+
 
 def has_uppercase(password):
     return any(char.isupper() for char in password)
 
+
 def has_lowercase(password):
-    return any (char.islower() for char in password)
+    return any(char.islower() for char in password)
+
 
 def has_number(password):
     return any(char.isdigit() for char in password)
+
 
 def has_special_character(password):
     special_characters = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/\\"
 
     return any(char in special_characters for char in password)
 
+
 def calculate_score(password):
 
     score = 0
 
     if check_length(password):
-        score += 1 
-    
+        score += 1
+
     if has_uppercase(password):
         score += 1
 
@@ -42,24 +47,22 @@ def calculate_score(password):
 
     return score
 
+
 def display_result(score):
 
     print("\nPassword Analysis")
 
-
     if score == 5:
-        print("Strength : Very Strong")
-
+        print("Strength : Very Strong 💪")
 
     elif score == 4:
-        print("Strength : Strong")
-
+        print("Strength : Strong ✅")
 
     elif score == 3:
-        print("Strength : Medium")
+        print("Strength : Medium ⚠️")
 
     else:
-        print("Strength : Weak")
+        print("Strength : Weak ❌")
 
 
 def main():
@@ -76,11 +79,11 @@ def main():
 
     print("\nChecklist")
 
-    print(" ✔ Minimum 8 characters : ", check_length(password))
-    print(" ✔ Uppercase letter : ", has_uppercase(password))
-    print(" ✔ Lowercase letter : ", has_lowercase(password))
-    print(" ✔ Number : ", has_number(password))
-    print(" ✔ Special character : ", has_special_character(password))
+    print("✔ Minimum 8 characters :", check_length(password))
+    print("✔ Uppercase letter      :", has_uppercase(password))
+    print("✔ Lowercase letter      :", has_lowercase(password))
+    print("✔ Number                :", has_number(password))
+    print("✔ Special character     :", has_special_character(password))
 
 
 if __name__ == "__main__":
