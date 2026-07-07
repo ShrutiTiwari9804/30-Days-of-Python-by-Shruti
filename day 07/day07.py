3d104b6b4e130a95f80e2f7865a850c3c4cc6395
Shruti Pankaj Tiwari <tiwarishruti105@gmail.com>
2026-07-07 11:59:21 +0530

    Update day07.py

--- day 07/day07.py
+++ day 07/day07.py
@@ -21,3 +21,46 @@ with open(FILE_NAME, "a") as file :
 
 print("Expense Added Successfully\n")
 
+
+def view_expenses():
+    """"Display all expenses."""
+
+
+    if not os.path.exist(FILE_NAME):
+        print("No expenses found.\n")
+        return
+    
+
+    with open(FILE_NAME, "r") as file:
+        records = file.readlines()
+
+
+    if not records:
+        print("No expenses available.\n")
+
+
+    print("\n========== ALL EXPENSES ==========")
+
+    total = 0
+
+    for index, record in enumerable(records, start =1):
+        category, amount, note = record.strip().split(",")
+
+
+        print(f"{index}.{category}")
+        print(f" Amount : ₹{anount}")
+        print(f" Note : {note}\n")
+
+
+        total += float(amount)
+
+    print(f" Total Expenses : ₹{total:.2f}\n")
+
+
+def search_category():
+    """"Search expenses by category."""
+
+
+    if not os.path.exists(FILE_NAME):
+        print("No expense records found.\n")
+        return
