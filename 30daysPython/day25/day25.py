033d599e1d3479844f81ed6b98525a798c555e29
Shruti Pankaj Tiwari <tiwarishruti105@gmail.com>
2026-07-25 19:19:13 +0530

    Update day25.py

---
+++ 30daysPython/day25/day25.py
@@ -0,0 +1,92 @@
+# FILE UPLOADS
+
+from fastapi import FastAPI, UploadFile, File
+
+app = FastAPI()
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type
+    }
+
+
+# FORM DATA
+
+from fastapi import Form
+
+@app.post("/login")
+def login(
+    username: str = Form(...),
+    password: str = Form(...)
+):
+    return {"username": username}
+
+
+
+# COOKIES- SET COOKIE
+
+from fastapi import Response
+
+@app.get("/set-cookie")
+def set_cookie(response: Response):
+    response.set_cookie(key="username", value="Shruti")
+    return {"message": "Cookie set"}
+
+
+# COOKIES- READ COOKIE
+
+
+from fastapi import Cookie
+
+@app.get("/get-cookie")
+def get_cookie(username: str = Cookie(None)):
+    return {"username": username}
+
+
+# RESPONSE MODEL
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+
+@app.get("/user/{id}", response_model=UserOut)
+def get_user(id: int):
+    return {
+        "id": 1,
+        "username": "Shruti",
+        "password": "secret"
+    }
+
+
+# STATUS CODES
+
+from fastapi import status
+
+@app.post("/create", status_code=status.HTTP_201_CREATED)
+def create():
+    return {"message":"Created"}
+
+
+
+# CUSTOM RESPONSE CLASSES- PLAIN TEST 
+
+from fastapi.responses import PlainTextResponse
+
+@app.get("/", response_class=PlainTextResponse)
+def home():
+    return "Hello"
+
+# CUSTOM RESPONSE- HTML
+
+from fastapi.responses import HTMLResponse
+
+@app.get("/html", response_class=HTMLResponse)
+def html():
+    return "<h1>Hello</h1>"
+
+
+
+
+
