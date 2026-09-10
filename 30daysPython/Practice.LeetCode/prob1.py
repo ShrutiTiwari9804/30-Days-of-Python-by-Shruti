deb915fe248d299fc0b4c74058f1888804f81fa7
Shruti Pankaj Tiwari <tiwarishruti105@gmail.com>
2026-09-10 14:28:44 +0530

    2 LeetCode easy problems solved.

--- 30daysPython/Practice.LeetCode/prob1.py
+++ 30daysPython/Practice.LeetCode/prob1.py
@@ -13,4 +13,17 @@ class Solution(object):
 
         return x == result
         
+# REVERSE A STRING 
+
+class Solution(object):
+    def reverseString(self, s):
         
+        left = 0
+        right = len(s)-1
+
+        while left < right :
+
+            s[left], s[right] = s[right], s[left]
+
+            left += 1
+            right -= 1    
