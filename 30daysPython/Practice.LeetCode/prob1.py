a4e115214bc4d670ca97ad8f5979254febef4c8d
Shruti Pankaj Tiwari <tiwarishruti105@gmail.com>
2026-09-17 13:37:50 +0530

    Update prob1.py

--- 30daysPython/Practice.LeetCode/prob1.py
+++ 30daysPython/Practice.LeetCode/prob1.py
@@ -27,3 +27,28 @@ class Solution(object):
 
             left += 1
             right -= 1    
+
+
+# VALID PALINDROME 
+
+class Solution(object):
+    def ispalindrome(self, s):
+
+        left = 0
+        right = len(s)-1
+
+        while left < right :
+
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True 
