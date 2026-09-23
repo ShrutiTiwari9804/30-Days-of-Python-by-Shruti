2b5037978ba85623d90632b15fc13cff8aa0c5de
Shruti Pankaj Tiwari <tiwarishruti105@gmail.com>
2026-09-23 11:13:24 +0530

    Update prob1.py

--- 30daysPython/Practice.LeetCode/prob1.py
+++ 30daysPython/Practice.LeetCode/prob1.py
@@ -52,3 +52,23 @@ class Solution(object):
             right -= 1
 
         return True 
+
+
+#count the digits that divide the number
+
+
+class Solution(object):
+    def countDigits(self, num):
+        
+        type num = int
+        rtype = int
+        original = num
+        count = 0
+        while num > 0 :
+            digit = num % 10
+            if  digit != 0 :
+                if  original % digit == 0 :
+                    count = count + 1
+                    
+            num = num // 10
+        return count 
